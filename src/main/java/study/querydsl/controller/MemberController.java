a8bd921f4002bc2b4d9e4502d4a192009243b8fb
sangoun94 <sangoun94@naver.com>
2021-05-20 16:40:13 +0900

    순수 JPA 에 QueryDSL 적용

---
+++ src/main/java/study/querydsl/controller/MemberController.java
@@ -0,0 +1,24 @@
+package study.querydsl.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.MembersearchCondition;
+import study.querydsl.repository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberv1(MembersearchCondition condition) {
+        return memberJpaRepository.search(condition);
+
+    }
+}
